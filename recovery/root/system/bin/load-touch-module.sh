526a51142e9042cc99c6957bebf8709a2a3cc01c
Rama Bondan Prakoso <ramanarubp@gmail.com>
2025-12-04 14:58:49 +0700

    fix(touchscreen): use stub_scp to resolve driver dependency
    The touchscreen driver requires a symbol from SCP which is effectively unused. Safely bypass this dependency using a stub module to ensure the driver loads correctly.
    
    Signed-off-by: Rama Bondan Prakoso <ramanarubp@gmail.com>

--- recovery/root/system/bin/load-touch-module.sh
+++ recovery/root/system/bin/load-touch-module.sh
@@ -1,21 +1,19 @@
 #!/sbin/sh
 # Touch module loader for tanzanite
 # Written by @linastorvaldz on GitHub
-# Last Update: 2025/11/02
+# Updated by @ramabondanp on GitHub
+# Last Update: 2025/12/05
 
 # disable exit-on-error
 set +e
 
 mod_dir="/lib/modules"
 mods=(
-  #mtk-mbox.ko # has no depends
   switch_class.ko # has no depends
   lct_tp.ko # has no depends
   mtk-afe-external.ko # has no depends
   xiaomi.ko # has no depends
-  #mtk_rpmsg_mbox.ko # depends on mtk-mbox
-  #mtk_tinysys_ipi.ko # depends on mtk-mbox and mtk_rpmsg_mbox
-  scp.ko # depends on mtk_tinysys_ipi , mtk-mbox, mtk-afe-external
+  stub_scp.ko # stub module to export scp_ipidev symbol
   ft3519t.ko # finally, load the touch driver after loading its dependencies
 )
 
